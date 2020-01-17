c5ad3090aaafad8741cb4ee4fcc15d321a02b91b
Nihal Agarwal <agarwalarrav@gmail.com>
2020-01-18 00:18:55 +0530

    intial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='windows-wifii-manager',
+    version='0.1.0',
+    packages=['Windows-Wi-Fi-Manager'],
+    url='https://github.com/NihalAgarwal/Windows-Wi-Fi-Manager',
+    license='MIT',
+    author='Nihal Agarwal',
+    author_email='nihal.agarwal.1426@gmail.com',
+    description='Know the saved WiFi details and add WiFi profile using a simple graphical interface.'
+)
